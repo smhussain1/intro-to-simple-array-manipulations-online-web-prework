d734f90080b63250604230d9e0e91636aed4ba7a
michael hussain <smhussain_1@yahoo.com>
2019-01-05 19:04:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,7 +10,8 @@ end
 def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza","Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   new_neighborhood = "Brooklyn Heights"
-  updated_array = neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+  #updated_array = 
+  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
 
 def using_pop(great_hits_of_the_nineties)
