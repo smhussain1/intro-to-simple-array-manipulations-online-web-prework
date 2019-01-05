476ea88530fba1aac8e3f817196426db1be9bb98
michael hussain <smhussain_1@yahoo.com>
2019-01-05 19:47:30 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,14 +1,18 @@
 def using_push(countries_in_western_africa, next_country)
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   next_country = "Niger"
-  updated_array = countries_in_western_africa.push(next_country)
-  updated_array
+   #countries_in_western_africa = 
+   countries_in_western_africa.push(next_country)
+  # updated_array = countries_in_western_africa.push(next_country)
+  # updated_array
+  countries_in_western_africa
 end
 
 def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza","Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   new_neighborhood = "Brooklyn Heights"
-  updated_array = neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+  #updated_array = 
+  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
 
 def using_pop(great_hits_of_the_nineties)
